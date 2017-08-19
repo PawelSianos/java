2d62597225ecbb4bc06ff216702a1d6bc737d64f
pawel.sianos <pawel.sianos@gmail.com>
2017-08-19 11:21:48 +0200

    First file

---
+++ CodeFights/add/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Paweł on 2017-08-19.
+ */
+public class Main {
+    public static void main(String... args){
+        int i = 6;
+        int j = 2;
+        System.out.println(add(i,j));
+    }
+    static int add(int param1, int param2) {
+        return param1 + param2;
+    }
+}
