2a23d7d00bf84b0feba4492767f44f030263a881
pawel.sianos <pawel.sianos@gmail.com>
2017-09-11 20:43:57 +0200

    11.09.2017(a)

--- Rozwiązanie/Zadanie 2 Rekurencja/src/Main.java
+++ Rozwiązanie/Zadanie 2 Rekurencja/src/Main.java
@@ -4,15 +4,15 @@
 public class Main {
     public static void main(String... args) {
         int[] tablica = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-        int dlugosc = tablica.length-1;
-        rekurencja(tablica, dlugosc);
+        int iloscElementow = tablica.length-1;
+        rekurencja(tablica, iloscElementow);
     }
 
-    private static void rekurencja(int[] tablica,int dlugosc) {
-        if(dlugosc >= 0){
-            System.out.println(tablica[dlugosc]);
-            dlugosc--;
-            rekurencja(tablica, dlugosc);
+    private static void rekurencja(int[] tablica,int iloscElementow) {
+        if(iloscElementow >= 0){
+            System.out.println(tablica[iloscElementow]);
+            iloscElementow--;
+            rekurencja(tablica, iloscElementow);
         }
     }
 }
